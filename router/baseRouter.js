8eaa849c352f830cd590455ff1cd6f9515faf28b
quinilo <hiquenvol@gmail.com>
2024-07-20 09:52:43 +0200

    missing files

---
+++ router/baseRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+function lang(code) {
+    return require('../lang/' + code + ".json");
+}
+
+router.get('/contact', (req, res) => {
+    res.render("contact", {lang: lang(req.lang)})
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require("express")
+const fs = require('fs');
 const path = require("path");
 const app = express()
+
+const applyRouter = require('./router/baseRouter')
+app.use('/:lang', (req, res, next) => {
+    req.lang = req.params.lang;
+    if (req.params.lang !== "en") if (req.lang !== "de") req.lang = "en"
+    next();
+});
+app.use('/:lang/', applyRouter);
+
+app.use('/', (req, res, next) => {
+    req.lang = "de";
+    next();
+});
+app.use('/', applyRouter);
 
 app.use(express.json())
 app.set("view engine", "ejs")
 app.set("case sensitive routing", false)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+function getFile(code) {
+    return require('./lang/' + code + ".json");
+}
+
+function lang(code) {
+    return require('./lang/' + code + ".json");
+}
+
+app.get('/', (req, res) => {
+    console.log("test")
+    res.render("index", {lang: lang(req.lang)})
+});
 
 app.listen(3000)
