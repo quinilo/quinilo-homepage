e75722f4d22ced63a189e5093ba664733dbc0083
quinilo <hiquenvol@gmail.com>
2024-07-27 14:54:57 +0200

    removed tests

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ const app = express()
 const baseRouter = require('./router/baseRouter')
 const tictactoeRouter = require('./router/tictactoeRouter')
 
-//testa
-//testt
-//test
-//test
-
 app.use('/:lang', (req, res, next) => {
     req.lang = req.params.lang;
     console.log(req.lang)
